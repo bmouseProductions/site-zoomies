5493613460c4834969310238679bde62224b6d55
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-09-21 11:02:22 -0300

    testando

--- src/assets/components/backTop/Index.tsx
+++ src/assets/components/backTop/Index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function BackTop(){
     const [isSticky, setIsSticky] = useState(false);
@@ -27,7 +28,7 @@ export default function BackTop(){
     return(
       <div id='backTop' className='fixed right-3 bottom-5 z-50'>
         <a href="#"  className='flex justify-center items-center rounded-full p-1 bg-[#ff7e00] border border-[#326ef5] shadow-[0px_0px_5px_#ffffff]'>
-          <ArrowUpwardIcon className='text-[30px] fill-white'/>
+          <FontAwesomeIcon icon={faArrowUp} />
         </a>
       </div>
     )
