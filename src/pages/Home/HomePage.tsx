a269d1533eb82286c6270ed35d33eaa56890a646
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-09-25 16:14:23 -0300

    shshshsh

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -122,30 +122,34 @@ export default function HomePage(){
                 </div>
             </section>
 
-            <section className='container mx-auto px-5 md:px-[5%] py-10 flex gap-10 flex-col lg:flex-row bg-[#f5a518]'>
-                <div className='lg:w-2/3 flex flex-col gap-7'>
-                    <h2 className='text-4xl lg:text-5xl font-semibold text-white'>Linha de Naturais</h2>
-                    <p className='text-[#1E1E1E] text-lg'>Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, porro exercitationem facilis et odit a ex in laudantium deserunt totam mollitia modi eaque corrupti repellendus repudiandae ipsa. Porro, harum eveniet! sit amet consectetur adipisicing elit. Quibusdam voluptas, dicta, maxime corrupti doloribus sint a, mollitia dolorem maiores eum error quo consequatur ad? Tempora tenetur iusto dolorum! Et, asperiores?</p>
-                    <Link to="/produtos" className='w-auto underline flex justify-center text-xl text-white font-semibold'>
-                        <button className='bg-[#04083a] py-3 px-10 rounded-2xl hover:bg-red-600 transition-all'>
-                            Ver todos os produtos
-                        </button>
-                    </Link>
+            <section className=' bg-[#f5a518]'>
+                <div className='container mx-auto px-5 md:px-[5%] py-10 flex gap-10 flex-col lg:flex-row'>
+                    <div className='lg:w-2/3 flex flex-col gap-7'>
+                        <h2 className='text-4xl lg:text-5xl font-semibold text-white'>Linha de Naturais</h2>
+                        <p className='text-[#1E1E1E] text-lg'>Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, porro exercitationem facilis et odit a ex in laudantium deserunt totam mollitia modi eaque corrupti repellendus repudiandae ipsa. Porro, harum eveniet! sit amet consectetur adipisicing elit. Quibusdam voluptas, dicta, maxime corrupti doloribus sint a, mollitia dolorem maiores eum error quo consequatur ad? Tempora tenetur iusto dolorum! Et, asperiores?</p>
+                        <Link to="/produtos" className='w-auto underline flex justify-center lg:justify-start text-xl text-white font-semibold'>
+                            <button className='bg-[#04083a] py-3 px-10 rounded-2xl hover:bg-red-600 transition-all'>
+                                Ver todos os produtos
+                            </button>
+                        </Link>
+                    </div>
+                    <img className='lg:w-[400px]' src={ilustracao} alt="" />
                 </div>
-                <img className='lg:w-[400px]' src={ilustracao} alt="" />
             </section>
 
-            <section className='container mx-auto px-5 md:px-[5%] py-10 flex gap-10 flex-col lg:flex-row-reverse bg-[#f5a518]'>
-                <div className='lg:w-2/3 flex flex-col lg:items-end gap-7'>
-                    <h2 className='text-4xl lg:text-5xl font-semibold text-white'>Linha de Naturais</h2>
-                    <p className='text-[#1E1E1E] text-lg'>Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, porro exercitationem facilis et odit a ex in laudantium deserunt totam mollitia modi eaque corrupti repellendus repudiandae ipsa. Porro, harum eveniet! sit amet consectetur adipisicing elit. Quibusdam voluptas, dicta, maxime corrupti doloribus sint a, mollitia dolorem maiores eum error quo consequatur ad? Tempora tenetur iusto dolorum! Et, asperiores?</p>
-                    <Link to="/produtos" className='w-auto underline flex justify-center text-xl text-white font-semibold'>
-                        <button className='bg-[#04083a] py-3 px-10 rounded-2xl hover:bg-red-600 transition-all'>
-                            Ver todos os produtos
-                        </button>
-                    </Link>
+            <section className=' bg-[#f5a518]'>
+                <div className='container mx-auto px-5 md:px-[5%] py-10 flex gap-10 flex-col lg:flex-row-reverse'>
+                    <div className='lg:w-2/3 flex flex-col lg:items-end gap-7'>
+                        <h2 className='text-4xl lg:text-5xl font-semibold text-white'>Linha de Naturais</h2>
+                        <p className='text-[#1E1E1E] text-lg'>Lorem, ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, porro exercitationem facilis et odit a ex in laudantium deserunt totam mollitia modi eaque corrupti repellendus repudiandae ipsa. Porro, harum eveniet! sit amet consectetur adipisicing elit. Quibusdam voluptas, dicta, maxime corrupti doloribus sint a, mollitia dolorem maiores eum error quo consequatur ad? Tempora tenetur iusto dolorum! Et, asperiores?</p>
+                        <Link to="/produtos" className='w-auto underline flex justify-center text-xl text-white font-semibold'>
+                            <button className='bg-[#04083a] py-3 px-10 rounded-2xl hover:bg-red-600 transition-all'>
+                                Ver todos os produtos
+                            </button>
+                        </Link>
+                    </div>
+                    <img className='lg:w-[400px]' src={ilustracao} alt="" />
                 </div>
-                <img className='lg:w-[400px]' src={ilustracao} alt="" />
             </section>
 
             <section className='container mx-auto px-5 md:px-[5%] mb-20'>
